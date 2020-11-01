2fe08e832421124fe121adf97be470897a80de08
Patricia Alves <patricia.18_08@hotmail.com>
2020-11-01 09:55:57 -0300

    InsertDados

--- src/main/java/br/com/cursoJsf/PersonBean.java
+++ src/main/java/br/com/cursoJsf/PersonBean.java
@@ -1,5 +1,39 @@
 package br.com.cursoJsf;
 
-public class PersonBean {
+import javax.faces.bean.ManagedBean;
 
+@ManagedBean(name = "personBean")
+public class PersonBean {
+				
+	private String name;
+	private String lastName;
+	private String fullName;
+	
+	public String showName() {
+		fullName = name + " "+lastName;
+		return "";
+	}
+			
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+	
+		
 }
