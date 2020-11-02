b81f0e12b96c802a6b8705b9344ba876fa2828da
Patricia Alves <patricia.18_08@hotmail.com>
2020-11-02 09:10:18 -0300

    Backing bean components and scoped

--- src/main/java/br/com/cursoJsf/PersonBean.java
+++ src/main/java/br/com/cursoJsf/PersonBean.java
@@ -1,38 +1,56 @@
 package br.com.cursoJsf;
 
-import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
+
+
+@ViewScoped
 @ManagedBean(name = "personBean")
+
 public class PersonBean {
 				
 	private String name;
-	private String lastName;
-	private String fullName;
+	private HtmlCommandButton commandButton;
 	
-	public String showName() {
-		fullName = name + " "+lastName;
+	private List<String> names = new ArrayList<String>();
+	
+	
+	public String addName() {
+		names.add(name);
+		
+		if (names.size() > 3) {
+			commandButton.setDisabled(true);
+		}
+		
 		return "";
 	}
+	public List<String> getNames() {
+		return names;
+	}
+	
+	
+	public void setNames(List<String> names) {
+		this.names = names;
+	}
 			
-	public String getFullName() {
-		return fullName;
-	}
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
-	public String getName() {
+	
+		public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
 	
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
 	
 	
 		
