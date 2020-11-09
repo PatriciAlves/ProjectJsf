07e789148e03104a097277dc07acb7c0a7ba24c4
Patricia Alves <patricia.18_08@hotmail.com>
2020-11-09 10:46:05 -0300

    Configuration hibernate

--- src/main/java/br/com/cursoJsf/PersonBean.java
+++ src/main/java/br/com/cursoJsf/PersonBean.java
@@ -12,7 +12,10 @@ import javax.faces.component.html.HtmlCommandButton;
 @ManagedBean(name = "personBean")
 
 public class PersonBean {
-				
+	
+	
+	private String password;
+	private String text;
 	private String name;
 	private HtmlCommandButton commandButton;
 	
@@ -24,11 +27,25 @@ public class PersonBean {
 		
 		if (names.size() > 3) {
 			commandButton.setDisabled(true);
-			return "navegationpage";
+			return "navegationpage?faces-redirect=true";
 		}
 		
 		return "";
 	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
 	public List<String> getNames() {
 		return names;
 	}
---
+++ src/main/java/br/com/entities/Person.java
@@ -0,0 +1,87 @@
+package br.com.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class Person implements Serializable {
+	
+	
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String lastname;
+	private Integer age;
+	private Date birth;
+	
+	public Person() {
+		
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+}
